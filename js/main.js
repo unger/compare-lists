5cf15e0ffc76e7355ec6968137b4a88f41fc3233
unger <magnus_unger@hotmail.com>
2021-01-04 13:30:48 +0100

    Added support for multicolumns

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $('#clean-button').on('click', function() {
 
 	function CleanArray(arr) {
 		for (var i = 0 ; i < arr.length ; i++) {
-			var val = arr[i];
-			val = val.replace(/\d*/, '');
-			arr[i] = jQuery.trim(val.toLowerCase());
+			var val = jQuery.trim(arr[i].toLowerCase());
+			if (val.length > 0) {
+				val = val[0].toUpperCase() + val.substring(1, val.length);
+			}
+			arr[i] = val;
 		}
 		for (var i = arr.length-1 ; i >= 0 ; i--) {
 			if (arr[i] === '') {
@@ -12,7 +14,6 @@ $('#clean-button').on('click', function() {
 			}
 		}
 	}
-
 
 	var text1 = $('#text1').val().split('\n');
 	var text2 = $('#text2').val().split('\n');
@@ -28,11 +29,38 @@ $('#compare-button').on('click', function() {
 
 	function ArrayContains(arr, value) {
 		for (var j = 0 ; j < arr.length ; j++) {
-			if (arr[j] === value) {
-				return true;
+			var val1 = arr[j];
+			var tabIndex1 = val1.indexOf('\t');
+			var val2 = value;
+			var tabIndex2 = val2.indexOf('\t');
+
+			if (tabIndex1 !== -1) {
+				val1 = val1.substring(0, tabIndex1);
+			}
+			if (tabIndex2 !== -1) {
+				val2 = val2.substring(0, tabIndex2);
+			}
+
+			if (val1 === val2) {
+				return arr[j];
 			}
 		}
-		return false;
+		return undefined;
+	}
+
+	function Combine(input1, input2) {
+		var values1 = input1.split('\t');
+		var values2 = input2.split('\t');
+
+		if (values2.length > 1) {
+			values2.splice(0, 1);
+		}
+
+		if (values2.length > 0) {
+			return values1.join('\t') + '\t' + values2.join('\t');
+		}
+		
+		return values1.join('\t');
 	}
 
 	var text1 = $('#text1').val().split("\n");
@@ -51,9 +79,10 @@ $('#compare-button').on('click', function() {
 	for (var i = 0 ; i < text1.length ; i++) {
 		var val1 = text1[i];
 		
-		if (ArrayContains(text2, val1)) {
-			if (!ArrayContains(equal, val1)) {
-				equal.push(val1);
+		var row2 = ArrayContains(text2, val1);
+		if (row2 !== undefined) {
+			if (ArrayContains(equal, val1) === undefined) {
+				equal.push(Combine(val1, row2));
 			}
 		} else {
 			missing2.push(val1);
@@ -63,9 +92,10 @@ $('#compare-button').on('click', function() {
 	for (var i = 0 ; i < text2.length ; i++) {
 		var val2 = text2[i];
 		
-		if (ArrayContains(text1, val2)) {
-			if (!ArrayContains(equal, val2)) {
-				equal.push(val2);
+		var row1 = ArrayContains(text1, val2);
+		if (row1 !== undefined) {
+			if (ArrayContains(equal, val2) === undefined) {
+				equal.push(Combine(val2, row1));
 			}
 		} else {
 			missing1.push(val2);
@@ -74,9 +104,9 @@ $('#compare-button').on('click', function() {
 
 	var template = _.template($('#rows-template').html());
 
-	$('#text1section').html(template({headline: 'Missing', rows: missing1}));
-	$('#text2section').html(template({headline: 'Missing', rows: missing2}));
-	$('#equalsection').html(template({headline: 'Equal', rows: equal}));
+	$('#text1section').html(template({headline: 'Missing (' + missing1.length + ')', rows: missing1}));
+	$('#text2section').html(template({headline: 'Missing (' + missing2.length + ')', rows: missing2}));
+	$('#equalsection').html(template({headline: 'Equal (' + equal.length + ')', rows: equal}));
 });
 
 
